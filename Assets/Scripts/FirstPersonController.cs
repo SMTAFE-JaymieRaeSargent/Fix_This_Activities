509bb8e997e927298994d85227e1606222f12be4
SMTAFE-JaymieRaeSargent <jaymie-rae.sargent@smtafe.wa.edu.au>
2026-08-10 15:40:58 +0800

    Modification: PlayerSetup
    Created player script

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    #region Variables 
+
+    #endregion
+
+    #region Player Movement
+
+    #endregion
+
+    #region Camera Control
+
+    #endregion
+
+    #region UnityEvents
+
+    #endregion
+}
